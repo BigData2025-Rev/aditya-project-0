a217a2b17b8e0f13f4b8947a69424466ca1ce9d3
walleaka <191993484+walleaka@users.noreply.github.com>
2024-12-22 23:08:52 -0800

    cleanup

--- csvfile.py
+++ csvfile.py
@@ -1,4 +1,6 @@
-import csv
+# csvfile.py
+# Aditya Kishan Ankaraboyana
+"""The main Csvfile object that helps parse and process CSVs."""
 import pandas as pd
 # from langchain_openai import OpenAI
 # from langchain.prompts import PromptTemplate
@@ -8,18 +10,21 @@ from data import Data
 
 
 class Csvfile(Data):
+    """Csvfile class"""
     def __init__(self, datasource):
         super().__init__(datasource=datasource)
         self.parse_datafile()
 
     def parse_datafile(self):
+        """Inherited from Data. Parses csv in this case."""
         try:
-            with open(self.datasource, "r") as csvfile:
+            with open(self.datasource, "r", encoding='utf-8') as csvfile:
                 self.df = pd.read_csv(csvfile)
         except Exception as e:
             print(e)
 
     def print_menu(self):
+        """Prints the menu."""
         print("What would you like to do ?")
         print("1. Generate summary report.")
         print("2. Print data columns.")
@@ -27,6 +32,10 @@ class Csvfile(Data):
         print("4. Exit\n")
 
     def get_sanitized_input(self):
+        """This method get input from the user and sanitizes it.
+           It also handles user input exceptions.
+        """
+
         while True:
             user_input = input("Please enter column names as comma separated values: ").strip()
 
@@ -46,21 +55,25 @@ class Csvfile(Data):
                 continue
 
     def generate_report(self):
-        print("Generate report function")
-        number_of_columns = len(self.df.columns)
+        """This method generates a prelimnary report for the given dataframe."""
+        print("Generate report function.\n")
         self.get_sanitized_input()
-        outputFileName = input("Enter file to generate report: ")
-        with open(outputFileName, "w") as file:
+        outputfilename = input("Enter file to generate report: ")
+        with open(outputfilename, "w", encoding='utf-8') as file:
             for column in self.df.columns:
-                file.write(f"For the column {column}, the highest occuring value is {self.df[column].mode().to_string()}.\n")
+                file.write(f"For the column {column}, the highest occuring value\
+                            is {self.df[column].mode().to_string()}.\n")
 
     def print_data_columns(self):
+        """Specialized to print data columns."""
         print("Print data function.")
 
     def create_custom_query(self):
+        """Use of custom queries to generate custom insights to data."""
         print("Create custom query function")
 
     def menu(self):
+        """Main menu for the Csvfile object. Handles input in REPL style."""
         while True:
             self.print_menu()
             user_input = input("Enter your input here: ")
